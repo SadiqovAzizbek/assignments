4ec482453c35b61712aa4e7c7cbacbf29bb49cbb
SadiqovAzibek <asadiqov244@gmail.com>
2025-10-08 10:40:29 +0500

    week 2 assignment

---
+++ week2assignment.py
@@ -0,0 +1,14 @@
+name = input("What is your name?")
+weight = int(input("Enter your weight (kg):"))
+height = float(input("Enter your height:"))
+age = int(input("Enter your age:"))
+gender = input("Enter your gender (M/F):")
+aedps = int(input("Enter your average exercise duration per session in minutes:"))
+pushups = int(input("Enter Push-ups in one set: "))
+squats = int(input("Enter Squats in one set: "))
+goal = int(input("What is your fitness goal? 1=Weight loss, 2=Muscle gain, 3=General fitness: "))
+print(f"\n{'='*50}") 
+
+
+
+
